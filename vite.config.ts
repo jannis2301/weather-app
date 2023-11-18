4f457be1b87e3a6404fd165cf130fb0006314a85
jannis2301 <jannis.ax@gmx.de>
2023-11-18 14:45:52 +0100

    chore: remove dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
-import dotenv from 'dotenv'
-dotenv.config()
 
 export default defineConfig({
   base: '/weather-app/',
-  define: {
-    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
-  },
 })
