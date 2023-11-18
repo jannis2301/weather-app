3d3e4de22524ad9f98932c22df14252e58d273e8
jannis2301 <jannis2301@users.noreply.github.com>
2023-11-18 04:33:30 +0000

    deploy: 70ad9d5743b9417462a6cd991e84355c14799be3

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Weather } from './Weather'
-
-const main = document.querySelector('.main') as HTMLElement
-
-const bootstrap = () => {
-  new Weather(main)
-}
-window.addEventListener('DOMContentLoaded', bootstrap)
