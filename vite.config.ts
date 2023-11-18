c0e4ba068afd12f185a33a1a4f7081a6893f0091
jannis2301 <jannis.ax@gmx.de>
2023-11-18 14:36:17 +0100

    refactor: add dotenv to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default defineConfig({
   base: '/weather-app/',
+  define: {
+    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+  },
 })
